83f80f13f761f59ec814b5c1e6af0f70269b5dfe
Alex Hughson <aehughson@gmail.com>
2024-01-20 22:32:08 +0000

    Include example of dropping into terminal with parsed data

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ import os
 import json
 
 from mgz.model import parse_match, serialize
-from mgz.summary import Summary
+from mgz.summary import Summary, FullSummary
+from IPython import embed
 
 matches = []
 
+testfile = "save-data/MP Replay v101.102.33868.0 @2024.01.18 222811 (4).aoe2record"
+
+# with open(testfile, 'rb') as testfile:
+#     sum = parse_match(testfile)
+#     embed()
+#     exit()
+
 for filename in os.listdir('save-data'):
-    if filename.endswith('.aoe2record'):
+    if filename.endswith('.aoe2record') and '2024' in filename:
         print(filename)
         with open('save-data/' + filename, 'rb') as f:
             try:
